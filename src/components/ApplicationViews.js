28930f8c33a2ad62e64497f5730ae969dea146d6
Lesley Boyd <lesley.a.boyd1@gmail.com>
2018-11-17 14:39:55 -0600

    data from api is showing, now need to add owners

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -5,24 +5,33 @@ import React, { Component } from "react"
 import AnimalList from './animal/AnimalList'
 import LocationList from './location/LocationList'
 import EmployeeList from './employee/EmployeeList'
-// import OwnerList from './owner/OwnerList'
+import OwnerList from './owner/OwnerList'
 
 
 export default class ApplicationViews extends Component {
     state = {
+        locations: [],
         animals: [],
-        employees: []
+        employees: [],
+        owners: []
     }
 
     componentDidMount() {
         const newState = {}
 
-        fetch("http://localhost:5002/animals")
+        fetch("http://localhost:5002/locations")
         .then(r => r.json())
+        .then(locations => newState.locations = locations)
+        .then(() => fetch("http://localhost:5002/animals")
+        .then(r => r.json()))
         .then(animals => newState.animals = animals)
         .then(() => fetch("http://localhost:5002/employees")
         .then(r => r.json()))
         .then(employees => newState.employees = employees)
+        .then(() => fetch("http://localhost:5002/owners")
+        .then(r => r.json()))
+        .then(owners => newState.owners = owners)
+        .then (() => this.setState(newState))
     }
 
     render() {
@@ -36,6 +45,9 @@ export default class ApplicationViews extends Component {
                 }} />
                 <Route exact path="/employees" render={(props) => {
                     return <EmployeeList employees={this.state.employees} />
+                }} /> 
+                <Route exact path="/owners" render={(props) => {
+                    return <OwnerList owners={this.state.owners} />
                 }} />   
             </React.Fragment>
         )
@@ -44,6 +56,3 @@ export default class ApplicationViews extends Component {
 
 
 
-/* <Route path="/owners" render={(props) => {
-                    return <OwnerList owners={this.state.owners} />
-                }} /> */
